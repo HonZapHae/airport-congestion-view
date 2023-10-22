47caa0165642e784f6b369fadac809be5a29a9d0
byungsker <extreme93@naver.com>
2023-10-22 21:31:09 +0900

    [features] Search Form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import Search from '../components/Search';
+// import CustomizedSwitches from '../components/CoutomSwitch';
 
 const StyledDiv = styled.div`
   flex: 1;
@@ -9,8 +11,24 @@ const StyledDiv = styled.div`
   justify-content: center;
 `;
 
+const AirPort = styled.div`
+  font-size: 32px;
+  margin-top: 120px;
+
+`;
+
 export function Home() {
+  const [selectedAirport, setSelectedAirport] = useState('김포국제공항');
+
+  const handleAirportSelect = (airportName: string) => {
+    setSelectedAirport(airportName);
+  };
+
   return (
-    <StyledDiv>홈(혼잡행) 페이지</StyledDiv>
+    <>
+      <Search onAirportSelect={handleAirportSelect} />
+      <AirPort>{selectedAirport}</AirPort>
+      <StyledDiv>홈(혼잡행) 페이지</StyledDiv>
+    </>
   );
 }
