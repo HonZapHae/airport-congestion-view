e6f09ddd501454700c6574d5e651ee170291fe42
byungsker <extreme93@naver.com>
2023-10-23 00:33:51 +0900

    [feat] Home → Search 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import Search from '../components/Search';
 // import CongestionDesc from '../components/home/CongestionDesc';
 
 const StyledDiv = styled.div`
@@ -24,10 +25,14 @@ export function Home() {
   };
 
   return (
-    <StyledDiv>
-      홈(혼잡행) 페이지
-      {/* memo:테스트용으로 해놓음  */}
-      {/* <CongestionDesc /> */}
-    </StyledDiv>
+    <>
+      <Search onAirportSelect={handleAirportSelect} />
+      <AirPort>{selectedAirport}</AirPort>
+      <StyledDiv>
+        홈(혼잡행) 페이지
+        {/* memo:테스트용으로 해놓음 */}
+        {/* <CongestionDesc /> */}
+      </StyledDiv>
+    </>
   );
 }
