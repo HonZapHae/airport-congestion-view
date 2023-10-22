16278bfadd51dd405c1decb094b715b093d1f852
sohee-an <ansoso634@gmail.com>
2023-10-22 20:13:03 +0900

    🩹 [fixed]공항 혼잡도 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-// import CongestionDesc from '../components/tip/CongestionDesc';
+import CongestionDesc from '../components/home/CongestionDesc';
 
 const StyledDiv = styled.div`
   flex: 1;
@@ -15,7 +15,7 @@ export function Home() {
     <StyledDiv>
       홈(혼잡행) 페이지
       {/* memo:테스트용으로 해놓음  */}
-      {/* <CongestionDesc /> */}
+      <CongestionDesc />
     </StyledDiv>
   );
 }
