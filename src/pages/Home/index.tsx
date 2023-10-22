b93fb7e1ca5d600ce5ff484fe38b1ab6e3276752
sohee-an <ansoso634@gmail.com>
2023-10-22 23:40:02 +0900

    ✨ [feat]  텍스트 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,7 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import * as Styled from './styled';
 // import CongestionDesc from '../components/home/CongestionDesc';
-
-const StyledDiv = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const AirPort = styled.div`
-  font-size: 32px;
-  margin-top: 120px;
-
-`;
 
 export function Home() {
   const [selectedAirport, setSelectedAirport] = useState('김포국제공항');
@@ -24,10 +11,13 @@ export function Home() {
   };
 
   return (
-    <StyledDiv>
+    <Styled.StyledDiv>
       홈(혼잡행) 페이지
       {/* memo:테스트용으로 해놓음  */}
       {/* <CongestionDesc /> */}
-    </StyledDiv>
+      <h2>국내선 탑승수속 소요시간안내</h2>
+      <span>탑승까지 평균 체류기간</span>
+      <Styled.Time>55분</Styled.Time>
+    </Styled.StyledDiv>
   );
 }
---
+++ src/pages/Home/styled.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const StyledDiv = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AirPort = styled.div`
+  font-size: 32px;
+  margin-top: 120px;
+`;
+export const Time = styled.h1`
+  font-size: 65px;
+`;
