f09663e127c56a9d543a5284696e875bace98b6e
byungsker <extreme93@naver.com>
2023-10-23 04:00:30 +0900

    [fix] Home

--- src/components/home/CongestionDesc/index.tsx
+++ src/components/home/CongestionDesc/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './styled';
 import Congestion from './Congestion';
 import { CONGESTION_DESC_INFO } from './constants';
+import Search from '../../Search';
 
 function CongestionDesc() {
+  const [selectedAirport, setSelectedAirport] = useState('김포국제공항');
+
+  const handleAirportSelect = (airportName: string) => {
+    setSelectedAirport(airportName);
+  };
   return (
     <Styled.Container>
       <Styled.CongestionWrapper>
+        <Search onAirportSelect={handleAirportSelect} />
+        {/* <AirPort>{selectedAirport}</AirPort> */}
         {CONGESTION_DESC_INFO.map(({ color, title }) => (
           <Congestion color={color} title={title} />
         ))}
