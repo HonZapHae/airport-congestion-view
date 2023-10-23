ec60918cd1d28e27d166e20cfc9c0a6b0ca4236c
hotdog1004 <hotdog87675210@gmail.com>
2023-10-23 18:14:20 +0900

    [chore] 폰트 전역 스타일 적용

--- src/styled.ts
+++ src/styled.ts
@@ -1,8 +1,15 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 export const Background = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+export const GlobalStyles = css`
+  body {
+    font-family: 'Noto Sans KR', 'Roboto', sans-serif;
+  }
 `;
